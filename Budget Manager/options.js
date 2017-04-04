73763e88562f6a79f0cae55b73dd9486e940aed6
Tanmay <tanmay2642@gmail.com>
2017-04-05 01:59:05 +0530

    Chrome Budget Manager extension

---
+++ Budget Manager/options.js
@@ -0,0 +1,30 @@
+$(function(){
+
+	chrome.storage.sync.get('limit', function(budget){
+		$('#limit').val(budget.limit);
+	})
+
+	$('#saveLimit').click(function(){
+		var limit = $('#limit').val();
+		if(limit)
+		{
+			chrome.storage.sync.set({'limit' : limit} , function(){
+				close();
+			});
+		}
+	});
+	$('#resetTotal').click(function(){
+		chrome.storage.sync.set({'total': 0} , function(){
+			var notifOption = 
+				{
+					type : 'basic',
+					iconUrl : 'icon48.png',
+					title : 'Toatal Reset!',
+					message : 'Total has been reset to 0'
+				};
+				chrome.notifications.create('limitNotif',notifOption);
+
+		});
+	});
+
+});
